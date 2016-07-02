a106a4568be638034b2b9fbe00c4c1135c972a35
Tom Koch <tomck@users.noreply.github.com>
2016-07-01 19:20:58 -0500

    Update juju2.rb 2.0 beta11

--- Formula/juju2.rb
+++ Formula/juju2.rb
@@ -3,7 +3,7 @@ class Juju2 < Formula
   homepage "https://jujucharms.com/"
 
   devel do
-    url "https://launchpad.net/juju-core/trunk/2.0-beta10/+download/juju-core_2.0-beta11.tar.gz"
+    url "https://launchpad.net/juju-core/trunk/2.0-beta11/+download/juju-core_2.0-beta11.tar.gz"
     sha256 "4aaaad717106d7b802a9d06ced1723d5a9a2837c831eb75deaec2bd574693ff9"
     version "2.0-beta11"
   end
